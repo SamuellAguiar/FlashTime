0dc821530405df09631dc45c6c5123c0e657a021
SamuellNote <samuellcarlosaguiar@gmail.com>
2023-09-20 12:38:55 -0300

    Final Update

--- Código/Assets/script.js
+++ Código/Assets/script.js
@@ -8,11 +8,11 @@ const relogio = setInterval(function time() {
     let min = dateToday.getMinutes();
     let s = dateToday.getSeconds();
 
-    if (hr < 10) hr = '0' + hr;
+    if (hr<10) hr = '0' + hr;
 
-    if (min < 10) min = '0' + min;
+    if (min<10) min = '0' + min;
 
-    if (s < 10) s = '0' + s;
+    if (s<10) s = '0' + s;
 
     horas.textContent = hr;
     minutos.textContent = min;
